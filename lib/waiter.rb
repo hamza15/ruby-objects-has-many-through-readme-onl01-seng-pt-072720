886b53073815b3edb9b348113eb400eb84e5a54c
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-28 01:55:27 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -13,5 +13,13 @@ class Waiter
     @@all
   end
 
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end
+  
+  def meals
+    Meal.all.select {|meal| meal.waiter == self}
+  end
+  
 
 end
