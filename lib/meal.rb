ac0954a7eac8fc35d054838d1cce3413ab7d6d39
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-28 01:48:15 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,19 @@
 class Meal
 
+  ttr_accessor :waiter, :customer, :total, :tip
+  @@all = []
+  
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+
+
+
 end
